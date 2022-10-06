7b28751ab1b5b74da7ddddde94436ee97ee50548
Adam Becker <ad@mbecker.cc>
2022-10-06 08:16:42 -0700

    xxlink, wbtc bribes

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const MAX_PERCENT = BigNumber(15)
 const MAX_BRIBE_IN_QI = QI_BRIBE_PER_ONE_PERCENT.times(MAX_PERCENT)
 
 const KNOWN_BRIBES_PER_ONE_PERCENT = {
-  [OUR_BRIBED_CHOICE]: QI_BRIBE_PER_ONE_PERCENT
-  // 'xxLINK (Polygon)': BigNumber(1000),
-  // 'WBTC (Metis)': BigNumber(1300),
+  [OUR_BRIBED_CHOICE]: QI_BRIBE_PER_ONE_PERCENT,
+  'xxLINK (Polygon)': BigNumber(600),
+  'WBTC (Metis)': BigNumber(600)
   // 'vGHST (Polygon)': BigNumber(901),
   // 'Beefy Aave ETH (Optimism)': BigNumber(1300),
   // 'WBTC (Optimism)': BigNumber(1000),
