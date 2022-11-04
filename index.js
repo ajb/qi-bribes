2e0b862a57b93587a847a74a173ccd54714b8c25
adamb <adamb0123@protonmail.com>
2022-11-04 06:12:59 -0700

    update ts

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const MAX_BRIBE_IN_QI = QI_BRIBE_PER_ONE_PERCENT.times(MAX_PERCENT)
 
 const KNOWN_BRIBES_PER_ONE_PERCENT = {
   [OUR_BRIBED_CHOICE]: QI_BRIBE_PER_ONE_PERCENT,
-  'xxLINK (Polygon)': BigNumber(500),
+  'xxLINK (Polygon)': BigNumber(550),
   'Beefy Aave ETH (Optimism)': BigNumber(500),
   'WBTC (Optimism)': BigNumber(550),
   'Yearn LINK (Ethereum)': BigNumber(600),
