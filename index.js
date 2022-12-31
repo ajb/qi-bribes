337b8af32f5f13510de4e4143c4fe3b6653e5b7c
adamb <adamb0123@protonmail.com>
2022-12-31 14:07:40 -0500

    match

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const GRAPHQL_ENDPOINT = 'https://hub.snapshot.org/graphql'
 const QIDAO_PROPOSAL_ID = '0xc202dec760f96d3f868a7353e1f50e2e51c38cd94e5fda0dabab8a50e70bf3a6'
 const TETU_REFLECTION_PROPOSAL_ID = '0xd19aa1308dc1bccef953f5b5aad6b64f880eb57ed91a821ad621f3c83e0b3ed5'
 const PAGE_SIZE = 1000
-const QI_BRIBE_PER_ONE_PERCENT = BigNumber(500)
+const QI_BRIBE_PER_ONE_PERCENT = BigNumber(600)
 const TETU_ADDRESS = '0x0644141DD9C2c34802d28D334217bD2034206Bf7'
 const MIN_PERCENTAGE_FOR_CHAIN_TO_RECEIVE_REWARDS = BigNumber('5')
 const TOTAL_WEEKLY_QI = BigNumber(150000)
@@ -20,7 +20,7 @@ const MAX_BRIBE_IN_QI = QI_BRIBE_PER_ONE_PERCENT.times(MAX_PERCENT)
 const KNOWN_BRIBES_PER_ONE_PERCENT = {
   [OUR_BRIBED_CHOICE]: QI_BRIBE_PER_ONE_PERCENT,
   'xxLINK (Polygon)': BigNumber(500),
-  'SAND (Polygon)': BigNumber(300),
+  'SAND (Polygon)': BigNumber(600),
   'Yearn LINK (Ethereum)': BigNumber(500)
 }
 
